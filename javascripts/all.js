73eddfefa3fb76d0ce3e8eedddde0622205599dd
JL Chen <azx011014@gmail.com>
2018-06-09 17:57:19 +0800

    Update 2018-06-09T09:57:11.184Z

---
+++ javascripts/all.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('return ' + helpers.getQuery());
+console.log('return ' + helpers.getQuery() + ' line 2');
+// ES6 範例程式碼
+
+var helpers = {
+  getQuery: function getQuery() {
+    console.log('getQuery');
+    return 'getQuery';
+  }
+};
+//# sourceMappingURL=all.js.map
